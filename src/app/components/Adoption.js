81906aebd1ce547a7f67abfe6c1f87cd9d31eca6
Morteza Araby <morteza.araby@gmail.com>
2017-01-23 18:55:15 +0100

    end of 03_03e

--- src/app/components/Adoption.js
+++ src/app/components/Adoption.js
@@ -3,14 +3,42 @@ import { connect } from 'react-redux'
 import Loader from 'Loader'
 
 class Adoption extends Component {
+    constructor(props) {
+        super(props)
+        
+    }
 
     adopt(a, i) {
         var imgSrc = "images/pets/" + a.filename + "_tn.jpg"
         var petName = a.pet + " photo"
+        //var dataTarget = '"#adoption' + a.filename +'"'
+        var targetId = "adoption" + a.filename 
+        var dataTarget="#adoption" + a.filename
         return (
-            <div className="col-md-2 col-sm-3 col-xs-4">
-                <img className="img-circle" src={imgSrc} alt={petName} key={i} />
+            <div key={i}>
+                <div className="col-md-2 col-sm-3 col-xs-4 openpetmodal" data-toggle="modal" onClick={() => {
+                    console.log("button click: ", dataTarget)
+                    $(dataTarget).modal('show')                    
+                }}>
+                    <img className="img-circle" src={imgSrc} alt={petName}/>
+                </div>{/*column*/}
+                <div className="modal fade" id={targetId} tabIndex="-1" role="dialog" aria-labelledby="petModal">
+                    <div className="modal-dialog" data-dismiss="modal" aria-label="Close" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <button type="button" className="close" data-dismiss="modal"  role="document"><span aria-hidden="true">&times;</span></button>
+                                <h2 className="modal-title modal-petname">{a.pet}</h2>
+                                <div className="modal-petbreed">{a.breed}</div>
+                            </div>{/*<!-- modal header -->*/}
+                            <div className="modal-body">
+                                <img className="img-responsive modal-petimage" src={imgSrc} alt={petName} />
+                                <p className="modal-petinfo">{a.description}</p>
+                            </div>{/*<!--modal-body-->*/}
+                        </div>{/*<!-- modal content -->*/}
+                    </div>{/*<!-- modal dialog -->*/}
+                </div>{/*<!-- modal fade -->*/}
             </div>
+
         )
     }
     render() {
