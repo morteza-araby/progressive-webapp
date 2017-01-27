0d566f4571894fb32dd7ebea568c3029ba859dfd
Morteza Araby <morteza.araby@ericsson.com>
2017-01-27 10:54:37 +0100

    add localhost cert&end of 04_02e

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -1,11 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 
 class Navigation extends Component {
+    componentDidMount() {
+        $('.reload').click(function () {
+            window.location.reload()
+        })
+    }
+      
     render() {
         return (
             <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
                 <div className="container">
                     <div className="navbar-header">
+                        <img className="reload reloadbtn-lg" src="images/icons/icon_reload.svg" alt="Reload Button" />
                         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
                             <span className="sr-only">Toggle navigation</span>
                             <span className="icon-bar"></span>
@@ -16,13 +23,14 @@ class Navigation extends Component {
                     </div>{/*<!-- navbar-header -->*/}
                     <div className="collapse navbar-collapse" id="collapse">
                         <ul className="nav navbar-nav navbar-right">
+                            <li><img className="reload reloadbtn" src="images/icons/icon_reload.svg" alt="Reload Button" /></li>
                             <li className="active"><a href="#slideshow">Home</a></li>
                             <li><a href="#adoption">Adoption</a></li>
                             <li><a href="#appointments">Appointments</a></li>
                         </ul>
                     </div>{/*<!-- navbar collapse -->*/}
                 </div>{/*<!-- navbar container -->*/}
-                {/*<!-- nav -->*/ }
+                {/*<!-- nav -->*/}
             </nav>
         );
     }
